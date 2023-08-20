ffda34de40659447404f8e1f317c1d0f4bd266d9
ljubicamuravljov <ljubicamuravljov@gmail.com>
2023-08-20 18:57:52 +0200

    Initial commit

---
+++ authentication/configuration.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+import os
+
+databaseUrl=os.environ["DATABASE_URL"] if os.environ["DATABASE_URL"] else "localhost:3308" #if "PRODUCTION" in os.environ else "local
+
+
+class Configuration():
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:123@{databaseUrl}/authentication"
+    JWT_SECRET_KEY="JWT_SECRET_KEY"
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES=timedelta(days=30)
---
+++ authentication/migrate.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from configuration import Configuration
+from flask_migrate import Migrate,init,migrate,upgrade
+from models import database, User
+from sqlalchemy_utils import database_exists,create_database
+
+application= Flask(__name__)
+application.config.from_object(Configuration)
+
+migrateObject = Migrate(application,database)
+
+
+#MOZDA OVO U JEDAN WHILE??? dont know
+
+if( not database_exists(application.config["SQLALCHEMY_DATABASE_URI"])):
+    create_database(application.config["SQLALCHEMY_DATABASE_URI"])
+
+database.init_app(application)
+
+with application.app_context() as context:
+    init()
+    migrate(message="Production migration")
+    upgrade()
+
+    admin = User(forename="Scrooge", surname="McDuck", email="onlymoney@gmail.com", password="evenmoremoney",
+                 role="owner")
+
+    database.session.add(admin)
+
+    database.session.commit()
+
+
+
